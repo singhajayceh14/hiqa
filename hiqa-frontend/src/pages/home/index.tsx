007b0148cac9240789ce3394fce0817017d00e07
Ashish <rajashishg@gmail.com>
2023-10-19 21:37:34 +0530

    Fix:Coming soon

---
+++ hiqa-frontend/src/pages/home/index.tsx
@@ -0,0 +1,16 @@
+import Home from '@/Layout/Home';
+
+function Index() {
+  return (
+    <>
+      <Home />
+    </>
+  );
+}
+Index.auth = false;
+
+Index.meta = {
+  title: 'Home',
+  description: 'Home',
+};
+export default Index;
